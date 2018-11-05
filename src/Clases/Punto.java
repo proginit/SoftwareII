5604bc162abc4b84d48163c170b39b27d84f7625
ProgInit <ProgInit@192.168.0.12>
2018-11-05 01:18:05 -0400

    actualizando datos

---
+++ src/Clases/Punto.java
@@ -0,0 +1,25 @@
+package Clases;
+
+/**
+ *
+ * @author ProgInit
+ */
+public class Punto {
+    public void Punto(){
+        
+    }
+    
+    public int Mas(int n){
+        return n = n+1;
+    }
+    
+    public int Menos(int Mas){
+        return Mas = Mas-1;
+    }
+    
+    public void Dato(int Mas, int Menos){
+        if (Mas <= 11){
+            System.out.println("Ganaste");
+        }
+    }
+}
